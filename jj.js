d415a4df37e3f43a2ecc710ee033f05cf71a25bf
Md Ariful <imdarif122@gmail.com>
2022-04-06 22:49:55 +0600

    My first

---
+++ jj.js
@@ -0,0 +1,35 @@
+const arrows = document.querySelectorAll(".arrow");
+const animeLists = document.querySelectorAll(".anime-list");
+
+arrows.forEach((arrow, i) => {
+    const itemNumbar = animeLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click", () => {
+        clickCounter++;
+        if (itemNumbar - (4 + clickCounter) >= 0) {
+            animeLists[i].getElementsByClassName.transform = `translateX(${
+                animeLists[i].computedStyleMap().get("transform")[0].x.value - 300
+            }px)`;
+        } else {
+            animeLists[i].style.transform = "translateX(0)";
+            clickCounter = 0;
+        }
+    });
+    console.log(animeLists[i].querySelectorAll("img").length);
+});
+
+
+
+
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+    ".container,.anime-list-item,.navbar,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+    ball.classList.toggle("active")
+})
